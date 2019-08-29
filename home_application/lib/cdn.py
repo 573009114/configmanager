221bcdfd146751cc9f3b29d5e41e7cdcfef9984c
573009114 <573009114@qq.COM>
2019-08-29 14:31:23 +0800

    12

--- home_application/lib/cdn.py
+++ home_application/lib/cdn.py
@@ -10,8 +10,8 @@ from qiniu import CdnManager
 
 def clear_qiniu(url):
     # 账户ak，sk
-    access_key = 'MeUQwQAvlOJ7C-wVcCsJIvq0PppRT3VrYKog34xv'
-    secret_key = 'wAwXfUYYD3vkHa_Qqi9S3F31RF6F8reIP7cWj-jj'
+    access_key = 'MeUQwQAvlOJ7C-wVcCsJIvq2122120PppRT3VrYKog34xv'
+    secret_key = 'wAwXf1212UYYD3vkHa_Qqi9S3F31RF6F8reIP7cWj-jj'
 
     auth = qiniu.Auth(access_key=access_key, secret_key=secret_key)
     cdn_manager = CdnManager(auth)
@@ -30,7 +30,7 @@ def yun_config():
     """
     Date=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
     AppID='040df59cfa41d034ce3dd8a7ebf6f1'
-    AppSecret='484a660445'
+    AppSecret='484a612121260445'
     MD5=md5.new()
     MD5.update('%s%s%s' %(Date,AppSecret,AppID))
     Sgn=MD5.hexdigest()
